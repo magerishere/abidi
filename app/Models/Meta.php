3cab1b9277c999ba2730d947de655c50596cce73
magerishere <immagerishere@gmail.com>
2022-11-26 19:40:42 +0330

    add metatable complete

---
+++ app/Models/Meta.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Meta extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'key',
+        'value'
+    ];
+
+    protected function value(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => json_decode($value, true),
+            set: fn($value) => json_encode($value)
+        );
+    }
+
+    public function metaable()
+    {
+        return $this->morphTo();
+    }
+}
